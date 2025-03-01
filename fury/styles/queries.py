4d09e53a83b02f0452895fd5d5eca13b00fefcba
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-01 20:13:40 +0000

    feat(deps): sanitize payload parser

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc2e832b408643eeb77b74f1c4e2fc27 = () => {
-  return '2025-02-13T03:14:44Z';
+const node_81853c9a689349a695d7d181eb857ca3 = () => {
+  return '2025-03-01T20:13:40Z';
 };
