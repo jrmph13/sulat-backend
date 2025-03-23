ccce6271962c31d8b08857100e7df460dff79a81
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-23 02:46:09 +0000

    refactor(core): enhance payload parser

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a33fb5caa39475fbcf0b88a38ca3ec8 = () => {
-  return '2025-03-18T22:42:27Z';
+const node_1161a2586b52431992ee6cbc420da202 = () => {
+  return '2025-03-23T02:46:09Z';
 };
