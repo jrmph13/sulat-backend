c2baef311683522fed11c1e753d762a41996d767
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 10:42:05 +0000

    chore(config): refactor view templates

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cc2a5116adb4726861922eb05d76f0d = () => {
-  return '2025-03-28T00:39:39Z';
+const node_ea367cca58fc42e8b2d06479154bae49 = () => {
+  return '2025-04-02T10:42:05Z';
 };
