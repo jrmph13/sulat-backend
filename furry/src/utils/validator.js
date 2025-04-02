0afdc6df03d41db0414af2fa824f389f7223044a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 03:07:23 +0000

    fix(core): fix middleware routes

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_234d2ca324634ab7bb81de6035057df4 = () => {
-  return '2025-03-14T19:54:50Z';
+const node_5964b95ee39840448b6a70a93b944dac = () => {
+  return '2025-04-02T03:07:23Z';
 };
