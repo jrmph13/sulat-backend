8028490422457d409d515436f62d66a7f4cd6796
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 17:09:23 +0000

    fix(config): sanitize caching layer

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be454478c6224f5087be64d7d59d2276 = () => {
-  return '2025-03-29T04:29:50Z';
+const node_9195212dd97c4fc0b6ebb89e1945e84a = () => {
+  return '2025-04-02T17:09:23Z';
 };
