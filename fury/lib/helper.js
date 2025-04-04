f7028b13f5f1bd452027556db3f8e42f9c043d5b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 03:04:49 +0000

    perf(core): sanitize database index

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd72729963724a7bbd87b05ab4f0ec8b = () => {
-  return '2025-03-06T10:49:07Z';
+const node_a37259cb333644ffb4a3468656c858cd = () => {
+  return '2025-04-04T03:04:49Z';
 };
