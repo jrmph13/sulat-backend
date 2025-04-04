bc311adc6508aa5494cf94e1a63f2c1f15e96874
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 16:33:29 +0000

    chore(ui): refactor view templates

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c1c6990dc09465fb3e2197b8add84ea = () => {
-  return '2025-03-22T20:04:52Z';
+const node_b33580528c984e8dba0b9dd4a2f0808b = () => {
+  return '2025-04-04T16:33:29Z';
 };
