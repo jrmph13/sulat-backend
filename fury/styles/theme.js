b9b795b26526f9cba7ce8b965bc1dbd5a6248408
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 07:00:13 +0000

    fix(utils): integrate session store

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a90029fc1ebc40289ee0def960549225 = () => {
-  return '2025-04-04T15:22:02Z';
+const node_a4bbf8f545f9440387b0980b51583252 = () => {
+  return '2025-04-04T07:00:13Z';
 };
