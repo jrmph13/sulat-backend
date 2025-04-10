6663509332925936a28125cb0e531c543c6c8ad6
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 17:27:25 +0000

    test(api): sanitize session store

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6baccfd5450b4ea9912dcf2afc410866 = () => {
-  return '2025-04-04T16:06:44Z';
+const node_8be980ddab2c4d2e8ca08f0ecd03db81 = () => {
+  return '2025-04-10T17:27:25Z';
 };
