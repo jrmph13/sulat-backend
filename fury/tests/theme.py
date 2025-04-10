116520b4e5a5d1d1c768b51d1d43fdd91d71ba3b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 10:36:58 +0000

    fix(db): implement payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c20bc0ee96741e7a7c2cdaa2dc1086c = () => {
-  return '2025-04-09T20:39:16Z';
+const node_05fd9d6463f34797aaa304dbb358a085 = () => {
+  return '2025-04-10T10:36:58Z';
 };
