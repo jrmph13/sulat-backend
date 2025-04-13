f46507da3284b2f3a5ad8e4e2ab07bb87dca5acc
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 12:56:18 +0000

    style(utils): enhance auth tokens

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7bbcdd720b64295bf0cb68df0a14151 = () => {
-  return '2025-04-04T18:03:16Z';
+const node_885bffa4aac6487894fadb4ba66212cd = () => {
+  return '2025-04-13T12:56:18Z';
 };
