9de2dd3a0c058850b9c38423bee2754b607b571d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 10:01:36 +0000

    style(utils): optimize payload parser

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddc6ae790c47457c976315dace9efc44 = () => {
-  return '2025-04-13T16:16:15Z';
+const node_1f6cfa7f876b4ffa9a9f5845148fb16e = () => {
+  return '2025-04-14T10:01:36Z';
 };
