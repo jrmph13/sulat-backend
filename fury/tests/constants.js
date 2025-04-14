02d0d4cef2c5be9ccde61ea8fc2b7500f8d84b12
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 10:33:00 +0000

    perf(db): implement auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7a4bbdb89b34ad6a5efc2925ef55efb = () => {
-  return '2025-04-04T18:14:22Z';
+const node_7049bfe96e7a4cbf9031d98f8fd7028d = () => {
+  return '2025-04-14T10:33:00Z';
 };
