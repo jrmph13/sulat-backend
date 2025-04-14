a1adf27cbd9365006763863de970b39b1efce8e2
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 03:41:30 +0000

    fix(ui): integrate view templates

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6e243045e8c4e6aae30773eeaca6f9f = () => {
-  return '2025-03-28T01:49:52Z';
+const node_92d9a2d760e748209816469108715589 = () => {
+  return '2025-04-14T03:41:30Z';
 };
