8d9c69e5ea24ee1a424e71f076f70bb981dcf24d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 18:13:45 +0000

    fix(core): fix payload parser

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b631c90bdcfe4bdeb09a4cb75962ec27 = () => {
-  return '2025-04-15T10:05:41Z';
+const node_59eedf8c464f46ca9f28412756d75bde = () => {
+  return '2025-04-15T18:13:45Z';
 };
