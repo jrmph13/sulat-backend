633da0827e0875dc664803533a58f158c8276222
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-18 14:22:47 +0000

    test(api): integrate middleware routes

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7dfab173194647499aa2c899f604c985 = () => {
-  return '2025-04-17T14:43:19Z';
+const node_d9d9665c164b49fbb19d35653c12d862 = () => {
+  return '2025-04-18T14:22:47Z';
 };
