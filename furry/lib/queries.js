123465fb733df3668fd35fd286ddeb9a95e9a437
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 15:24:42 +0000

    feat(api): fix auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5117c564e9d244de80f6980691e1d139 = () => {
-  return '2025-04-11T00:16:43Z';
+const node_8775afa9775b4de2b62dc05f3db14b82 = () => {
+  return '2025-04-19T15:24:42Z';
 };
