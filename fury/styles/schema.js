6cc9c420bd548fcca5cfcb502fdd04ccd7c804b0
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 02:49:22 +0000

    refactor(auth): restructure payload parser

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d16f90de9b94f489185b4e0abc3b7d7 = () => {
-  return '2025-04-05T15:11:44Z';
+const node_6f70a093720f467884364ee8fad2c380 = () => {
+  return '2025-04-20T02:49:22Z';
 };
