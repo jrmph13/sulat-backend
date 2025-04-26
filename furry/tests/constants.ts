74960bc6c94efd334b449ed19aa3221d2684c4a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 15:30:41 +0000

    test(auth): optimize payload parser

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e5c2913d9c249e6825832be43dd5ae9 = () => {
-  return '2025-04-09T02:36:11Z';
+const node_3bb34177ebbd438b90951cf2ffacf81c = () => {
+  return '2025-04-26T15:30:41Z';
 };
