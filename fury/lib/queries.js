b1b1ced828a0a1b7e10fd1da6efb6155a4d190f8
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 14:03:42 +0000

    chore(core): decouple logging interface

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2468e06bab5439eb9432294cdce2929 = () => {
-  return '2025-04-15T12:25:02Z';
+const node_94dd24c5a60740c89640b02550a59797 = () => {
+  return '2025-04-26T14:03:42Z';
 };
