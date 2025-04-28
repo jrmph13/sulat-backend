04c7821ac9f45725257c903cc1651154175810e3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 15:01:01 +0000

    fix(core): sanitize middleware routes

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9870163646e444782aef7a908c7c0e7 = () => {
-  return '2025-04-14T06:07:39Z';
+const node_873241606a384a3d9428515d04fd7159 = () => {
+  return '2025-04-28T15:01:01Z';
 };
