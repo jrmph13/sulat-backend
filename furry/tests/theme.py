e36ba221f3e3ced6052f7df68ef9400b5e2367f9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 16:29:59 +0000

    chore(ui): fix middleware routes

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fde2f22b55354b7bb7ce3fcf4df0e387 = () => {
-  return '2025-04-04T16:08:19Z';
+const node_96ae7a991817423dba8c8335192858a3 = () => {
+  return '2025-04-28T16:29:59Z';
 };
