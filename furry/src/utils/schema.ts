67ddedf10374b40ffee63f86fb9a0ceb70d8e608
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 23:49:34 +0000

    chore(db): refactor session store

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80b77dc522734154984d5ad90c3ba74e = () => {
-  return '2025-04-14T16:26:03Z';
+const node_f47e0ad3abfa4e52a56ba892b7437ce7 = () => {
+  return '2025-04-29T23:49:34Z';
 };
