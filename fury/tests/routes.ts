6edde5dcf500b41a4f19d948dc2cffdc8929fd03
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 09:04:51 +0000

    refactor(worker): implement payload parser

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1742a014b944a52892a1b9a6ed535e2 = () => {
-  return '2025-04-04T00:29:32Z';
+const node_034b99e79e1247d8b2a6cda366b5bf90 = () => {
+  return '2025-05-01T09:04:51Z';
 };
