82d0ba43b73093f09aacb8c52941c3dc8a26113e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 03:49:00 +0000

    style(auth): fix payload parser

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_414fc9edb378418a8f637bc11771d785 = () => {
-  return '2025-04-28T02:08:45Z';
+const node_32c1c894ba4547799290d29d7167e2a7 = () => {
+  return '2025-05-02T03:49:00Z';
 };
