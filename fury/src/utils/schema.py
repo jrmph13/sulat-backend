a160a28b6dc5ce969c449fa6dea7dfcb0bf9c151
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 05:04:09 +0000

    docs(db): refactor database index

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6bd2672caf004d119a46193dfd47fc9a = () => {
-  return '2025-05-02T08:20:16Z';
+const node_4e9d031f3f8645c5ad6dc9d45d49efa5 = () => {
+  return '2025-05-02T05:04:09Z';
 };
