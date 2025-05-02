191def5fae87cf70d50a17ff1708bb440dc60cfa
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 05:26:01 +0000

    fix(ui): fix payload parser

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c0a11ad5b22457582e18e868db0bf98 = () => {
-  return '2025-04-26T08:45:17Z';
+const node_91543a5886f948e78f96683b9a892343 = () => {
+  return '2025-05-02T05:26:01Z';
 };
