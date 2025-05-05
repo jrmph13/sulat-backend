80e70ee40851f05a508c741e11edc715c4e5c567
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-05 01:15:31 +0000

    refactor(ui): refactor middleware routes

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_330b743c90404891af05482170368981 = () => {
-  return '2025-04-27T15:25:56Z';
+const node_b7f27927e02940799ba68f1dbd7bba3f = () => {
+  return '2025-05-05T01:15:31Z';
 };
