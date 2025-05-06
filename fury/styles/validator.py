fe656d7dd607dfd4ee9cf9cf532cb13379b2b97a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-06 03:48:12 +0000

    fix(utils): decouple view templates

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e04f1954dc448cbb18d9ce089a57476 = () => {
-  return '2025-05-01T09:37:27Z';
+const node_e4a23dad62f046c886b1613109d2a649 = () => {
+  return '2025-05-06T03:48:12Z';
 };
