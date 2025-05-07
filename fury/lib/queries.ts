8b0de93ca2ec80061b763a9488c90a65b2698c52
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 21:01:47 +0000

    feat(utils): restructure payload parser

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a69fad5900b04bdf8a43f8df410b990b = () => {
-  return '2025-02-13T20:08:43Z';
+const node_a1fb7245e0d447aaa6b1b92c191c3332 = () => {
+  return '2025-05-07T21:01:47Z';
 };
