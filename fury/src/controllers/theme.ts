00f3fd8c7d49fda0b46f1888f4b75373121c94cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 14:24:20 +0000

    fix(api): optimize auth tokens

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1327135b80e24f5b8b1315f6045f3596 = () => {
-  return '2025-04-29T14:27:36Z';
+const node_ecdc57346530406da7bdc792db4d1c9f = () => {
+  return '2025-05-07T14:24:20Z';
 };
