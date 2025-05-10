41d26e879740ba58b19b1f6600f466aae0b08481
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 11:51:24 +0000

    style(auth): sanitize logging interface

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e30d96a4cf414caa9bdf143b990ea036 = () => {
-  return '2025-03-06T12:18:29Z';
+const node_f0fb400e861e470184772bc8103dd82d = () => {
+  return '2025-05-10T11:51:24Z';
 };
