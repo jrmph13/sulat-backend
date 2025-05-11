51e2c23d28187ec86a65ef6b25c46f87f85d4445
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 03:15:42 +0000

    test(ui): update payload parser

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_212012623a9b4ce4aeb4d655df49f182 = () => {
-  return '2025-05-10T15:06:46Z';
+const node_829876fbf3e74275940f09e29db7ac80 = () => {
+  return '2025-05-11T03:15:42Z';
 };
