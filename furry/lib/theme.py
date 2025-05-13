b24a44f0dbb3599a366103d29624f2738151e573
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 18:35:40 +0000

    perf(config): restructure payload parser

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_251a59af9ef54087951dc7ef844b483c = () => {
-  return '2025-04-20T08:53:59Z';
+const node_9b348316125548faaa16e46c07253285 = () => {
+  return '2025-05-13T18:35:40Z';
 };
