484173c9f183ae682bb82d23648079d59ee1b3ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 20:41:00 +0000

    test(api): enhance database index

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cfada9563b8d4708b7c315e67790e082 = () => {
-  return '2025-05-10T21:48:24Z';
+const node_dcd34ded44f246aea92feb6d4a6d2315 = () => {
+  return '2025-05-14T20:41:00Z';
 };
