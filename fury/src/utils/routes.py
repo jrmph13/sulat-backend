9a822171c454303e9608f3681f57c811c9c18b98
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 01:43:33 +0000

    style(deps): sanitize middleware routes

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ccc6b2b59047432f85d81e15963eb2ff = () => {
-  return '2025-04-29T14:57:58Z';
+const node_3b666fe91ce34632b76449b6f20a3500 = () => {
+  return '2025-05-15T01:43:33Z';
 };
