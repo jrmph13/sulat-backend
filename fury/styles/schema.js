ae455172af825112ac62dd6a90f6c6bb4d1ae4fa
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 13:30:45 +0000

    refactor(worker): update session store

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f70a093720f467884364ee8fad2c380 = () => {
-  return '2025-04-20T02:49:22Z';
+const node_74ed41b038d64e7999707e259786e8b0 = () => {
+  return '2025-05-15T13:30:45Z';
 };
