586fbf05b715ad12b39b0b5e66b06df2cfcc8f4e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 15:34:13 +0000

    fix(api): refactor middleware routes

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_482f59d895ea433eb7f924ac27c8fac8 = () => {
-  return '2025-04-18T17:14:26Z';
+const node_81d5c9460a27489e9340b01ee28000f9 = () => {
+  return '2025-05-19T15:34:13Z';
 };
