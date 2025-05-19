a46a73447d5a173de4c6e07b719105aa5cd052b1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 19:50:05 +0000

    refactor(deps): restructure auth tokens

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcd34ded44f246aea92feb6d4a6d2315 = () => {
-  return '2025-05-14T20:41:00Z';
+const node_62ca614180604601af278f1f4485ca6f = () => {
+  return '2025-05-19T19:50:05Z';
 };
