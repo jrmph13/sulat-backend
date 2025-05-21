650654fb1e885dc9427b8af8c437994c461ddf1e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-21 03:28:52 +0000

    feat(auth): fix database index

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81b8514abb69466ea779e5ce8a8d72a7 = () => {
-  return '2025-05-10T09:15:56Z';
+const node_6bd5f97fbfba452987457116cd1e124f = () => {
+  return '2025-05-21T03:28:52Z';
 };
