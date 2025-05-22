7e54d442c778306b852d003aa2c55e309f93d7a8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 06:04:36 +0000

    perf(api): optimize view templates

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db669d107476400db36953ddba1fc005 = () => {
-  return '2025-05-11T05:18:36Z';
+const node_4c8872799e20419ea60227891fecc55a = () => {
+  return '2025-05-22T06:04:36Z';
 };
