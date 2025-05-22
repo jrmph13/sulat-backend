16a723973e6fe6b02e4d18fe988b4cf76b5fbd32
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 11:46:20 +0000

    fix(deps): optimize session store

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_188eff3dd65743309c334e6416ba1fad = () => {
-  return '2025-05-02T23:51:43Z';
+const node_e247ed9f3eba47c2b60b6968dbff4ac4 = () => {
+  return '2025-05-22T11:46:20Z';
 };
