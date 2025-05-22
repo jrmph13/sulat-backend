081373c6bcec35b2e5bce28a003197885cc081a0
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 03:33:58 +0000

    fix(db): implement auth tokens

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e1729cf29664888b81b5c0ab63f97af = () => {
-  return '2025-05-19T10:50:01Z';
+const node_cdf32422d8654c749e3c4591f2aa5d16 = () => {
+  return '2025-05-22T03:33:58Z';
 };
