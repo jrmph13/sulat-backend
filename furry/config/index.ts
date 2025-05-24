31e62799a24982aabc440c73b68b35d15874a040
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 09:48:32 +0000

    fix(core): fix session store

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9943e7f956db4991b6d1fe2e2b3d8b3a = () => {
-  return '2025-05-14T00:18:08Z';
+const node_0c1deab75f0946c2926fa7f28f1ad533 = () => {
+  return '2025-05-24T09:48:32Z';
 };
