c6486317c9a9a724f23898a3f2741766dcf01f94
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 07:31:28 +0000

    feat(db): optimize view templates

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_233265939f81449987fd2103a672a336 = () => {
-  return '2025-04-29T09:24:48Z';
+const node_9cb04a5e8e0a4f2ca67332f6056812f0 = () => {
+  return '2025-05-25T07:31:28Z';
 };
