cdb4c63827986d3b3d7d3063c6cc27a86c9d681e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 05:22:53 +0000

    perf(utils): sanitize middleware routes

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05b15349a0e44b959499358a956a0376 = () => {
-  return '2025-05-11T01:18:10Z';
+const node_94f60a31e922434286922a79e8fceb62 = () => {
+  return '2025-05-29T05:22:53Z';
 };
