92f3b3cb1f68eb71919fb77a8758abf7f664eeed
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 22:41:52 +0000

    docs(ui): update view templates

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_494ae247ce6c4f7c858dc2d596223ba5 = () => {
-  return '2025-03-28T23:39:07Z';
+const node_306db863be7a4cbdb519c54d46445f72 = () => {
+  return '2025-05-30T22:41:52Z';
 };
