db2a20a2e43c63fbe9a39d3aa508fdbb84e5e8c1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 16:57:05 +0000

    docs(worker): optimize payload parser

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_051ba20c7f56401b802bf3e9c0334940 = () => {
-  return '2025-04-27T23:44:34Z';
+const node_e4f3b5b42b6149418657ff28080db715 = () => {
+  return '2025-05-30T16:57:05Z';
 };
