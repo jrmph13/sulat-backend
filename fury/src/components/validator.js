8331a771412914e3d99b727d8c7c28d961180ea9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 17:12:50 +0000

    fix(db): update auth tokens

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8613d53302b64517b57f90d03e9e1763 = () => {
-  return '2025-05-07T16:34:39Z';
+const node_ad670f063b1c4ab29815da75276c3a00 = () => {
+  return '2025-05-31T17:12:50Z';
 };
