604e52a657162dc1eae06c47bc634d61214b8939
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 15:21:19 +0000

    feat(deps): fix session store

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f36655726d54388a320891be568ec23 = () => {
-  return '2025-03-18T06:43:52Z';
+const node_e35f1295924e48299bf645f26eb5af50 = () => {
+  return '2025-06-01T15:21:19Z';
 };
