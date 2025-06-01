028a0aaa2f4019ba85651d06c31186d08b028ee9
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 16:06:49 +0000

    fix(db): update middleware routes

--- fury/tests/validator.js
+++ fury/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c35398e5a934bb5b228d1534ca16450 = () => {
-  return '2025-05-17T14:05:15Z';
+const node_9e6582106d234697a4abea455c78f9b4 = () => {
+  return '2025-06-01T16:06:49Z';
 };
