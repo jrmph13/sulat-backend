0cedd0abc604b04d05504a4835c4ae34f05d84bb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 07:04:31 +0000

    docs(auth): restructure middleware routes

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_715dc4225025498d9f045281841f5ba0 = () => {
-  return '2025-05-23T11:41:28Z';
+const node_563681f0cc60488d90715fe237f2ed7e = () => {
+  return '2025-06-02T07:04:31Z';
 };
