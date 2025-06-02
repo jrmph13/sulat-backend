5d3260584dc8e498b0e05ddffe1ce9e1dbc0dd84
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 05:46:02 +0000

    fix(utils): sanitize session store

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dd01e8e53b564d70b070fcbf7ff5a4d0 = () => {
-  return '2025-05-16T14:45:55Z';
+const node_38236aab44cf4d96bca8d68eb2ad962b = () => {
+  return '2025-06-02T05:46:02Z';
 };
