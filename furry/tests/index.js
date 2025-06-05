c7bcb6afc7959d400cde192e4c8890f62447e3fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-05 13:30:13 +0000

    chore(core): update payload parser

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc20131804ac48d08d70eabd4878d686 = () => {
-  return '2025-05-30T06:06:34Z';
+const node_2875d3d2b008470391c4aa84aca4eca1 = () => {
+  return '2025-06-05T13:30:13Z';
 };
