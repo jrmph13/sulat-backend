d9b1cc1654e0574784a4a856a5b4b41d2dea4d29
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 20:13:16 +0000

    refactor(api): restructure database index

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6d91ae5831b4b4d94cdc95df2320179 = () => {
-  return '2025-04-17T11:16:58Z';
+const node_559acd9bee0e41d9a28970795b7aa8dc = () => {
+  return '2025-06-06T20:13:16Z';
 };
