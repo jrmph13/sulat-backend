fb737895cbd69d7bd1863ab29f54a137e2d8a8e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 18:18:35 +0000

    perf(config): restructure view templates

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_00a938cfc4ad4dad8e0958f8e3f6a095 = () => {
-  return '2025-05-28T03:53:58Z';
+const node_1e4d15f891b945418618f511c10f5324 = () => {
+  return '2025-06-07T18:18:35Z';
 };
