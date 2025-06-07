fbd170f7380edd09e12e35bc9b8f5b185b40da3b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 22:33:25 +0000

    feat(config): fix payload parser

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2ec6561036d40d09504d0ae55a58788 = () => {
-  return '2025-06-06T00:28:30Z';
+const node_4b2069f1401940efb6fcef997cde6df9 = () => {
+  return '2025-06-07T22:33:25Z';
 };
