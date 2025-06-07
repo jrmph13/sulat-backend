cb802568f979774e84b0de045101a2058e09f7fa
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 23:35:08 +0000

    fix(worker): fix session store

--- furry/tests/schema.js
+++ furry/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05eeaf308c1346c790c459c9f6e23a98 = () => {
-  return '2025-05-04T16:06:34Z';
+const node_c7757c71325c47e3ac2897b57bf09867 = () => {
+  return '2025-06-07T23:35:08Z';
 };
