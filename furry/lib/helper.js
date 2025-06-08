94584d2093066959455e4b4c4ff2d18090922410
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 05:43:52 +0000

    refactor(core): optimize payload parser

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa3251fa5116415895e1ab65188fcf9b = () => {
-  return '2025-04-13T20:06:12Z';
+const node_e911ae8893644affb1ff91561cf09a6e = () => {
+  return '2025-06-08T05:43:52Z';
 };
