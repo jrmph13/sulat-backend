4c02114a492f4157cdc0a958fa9336f13f57dae6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 13:09:19 +0000

    perf(core): fix auth tokens

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40743b4cbf5a41f2a047e3c836f7376a = () => {
-  return '2025-05-16T11:29:12Z';
+const node_383683cc74fd4ca18fb695a06d056b7a = () => {
+  return '2025-06-08T13:09:19Z';
 };
