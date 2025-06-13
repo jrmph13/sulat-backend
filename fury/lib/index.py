bf161051661a2d7976387ddb6ad07d5e3a21efeb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 21:34:22 +0000

    fix(config): update payload parser

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b32feebf494465a9ba4bde8dc32f0b1 = () => {
-  return '2025-06-08T08:04:30Z';
+const node_d26c1ea85dc34a5992db3748890e748b = () => {
+  return '2025-06-13T21:34:22Z';
 };
