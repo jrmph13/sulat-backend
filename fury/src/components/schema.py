b6ae8df87b52ecc5ee7947c0f78e845948f8262d
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 03:28:07 +0000

    refactor(utils): decouple view templates

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41cc2c201a7046ccbbd4f06738c3262b = () => {
-  return '2025-06-01T18:27:28Z';
+const node_1c764f65e4a9467d8249af15170c617c = () => {
+  return '2025-06-13T03:28:07Z';
 };
