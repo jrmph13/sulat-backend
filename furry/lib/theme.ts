d6e1184b8e4bef3663c7b4af9191f88c114c2cfb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 08:04:34 +0000

    fix(auth): integrate database index

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7277f721a30442494d5c0a0bb211b1a = () => {
-  return '2025-04-29T17:00:55Z';
+const node_be30cb4916f8438eaeea77b187440b83 = () => {
+  return '2025-06-14T08:04:34Z';
 };
