da71d668ddd6bfb350511b965a49acce396575aa
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 22:21:04 +0000

    docs(deps): integrate auth tokens

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5713978cd1342eabe63adeefd895460 = () => {
-  return '2025-06-03T15:37:35Z';
+const node_decf22aa081b41d1b759f77791309408 = () => {
+  return '2025-06-21T22:21:04Z';
 };
