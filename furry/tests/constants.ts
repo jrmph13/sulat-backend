120485594bcf1c28fd00a11c30d56b8bb7aae23d
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 06:21:42 +0000

    test(api): restructure validation pipeline

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bb34177ebbd438b90951cf2ffacf81c = () => {
-  return '2025-04-26T15:30:41Z';
+const node_b16163c8e737476ca9457ea475f65433 = () => {
+  return '2025-06-21T06:21:42Z';
 };
