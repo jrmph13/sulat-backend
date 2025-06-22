d5dd75d34eb9bbc54a5c159d0b5c4c81358c73b1
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 07:35:28 +0000

    refactor(utils): restructure middleware routes

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_499f81d663ab43e492c0b00edbef979a = () => {
-  return '2025-04-07T13:13:07Z';
+const node_0675925e56ff4f17adf5cf95ce56eca0 = () => {
+  return '2025-06-22T07:35:28Z';
 };
