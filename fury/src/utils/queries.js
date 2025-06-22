beaf81aee6e6234dd9381e2db5e67b4f3661f60e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 06:07:08 +0000

    chore(utils): sanitize auth tokens

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad7c7eed9c194eb381e62b52d6b71ca4 = () => {
-  return '2025-06-06T01:06:03Z';
+const node_313189a093ac4f988ca1104d6c49c14a = () => {
+  return '2025-06-22T06:07:08Z';
 };
