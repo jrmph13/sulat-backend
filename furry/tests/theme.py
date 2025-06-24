c7b1622bcbec41e65faf1419901cd85f3095d69c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 07:28:07 +0000

    docs(auth): restructure middleware routes

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96ae7a991817423dba8c8335192858a3 = () => {
-  return '2025-04-28T16:29:59Z';
+const node_bf47121a60a04ed29eff7971e1c05b26 = () => {
+  return '2025-06-24T07:28:07Z';
 };
