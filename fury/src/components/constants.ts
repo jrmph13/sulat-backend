47536ee55c9792b853433dcb3388696c407f2260
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 21:30:35 +0000

    fix(worker): enhance auth tokens

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9dffd802a9ae4735b1fbab8ae78dc3ba = () => {
-  return '2025-04-12T06:42:39Z';
+const node_584b0d88e7d944d592bbaf779335e3e1 = () => {
+  return '2025-06-25T21:30:35Z';
 };
