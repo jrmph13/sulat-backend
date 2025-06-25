643020ef46cb888068b3f410e2ac74f2f951fabd
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 19:09:30 +0000

    feat(ui): restructure middleware routes

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91543a5886f948e78f96683b9a892343 = () => {
-  return '2025-05-02T05:26:01Z';
+const node_096ddb4b5eb94a3f98fc2f7810eade2a = () => {
+  return '2025-06-25T19:09:30Z';
 };
