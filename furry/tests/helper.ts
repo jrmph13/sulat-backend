c8a0e1db9191e03742bb4e0ab8af11ac51715da3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 12:51:40 +0000

    test(auth): restructure logging interface

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08c3eb19f5a84b1ea20aa7f58f26a9c7 = () => {
-  return '2025-06-24T18:56:48Z';
+const node_9c1f373a909b4958bddadab1da0a8ecb = () => {
+  return '2025-06-27T12:51:40Z';
 };
