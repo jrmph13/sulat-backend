f407be42e1c2346e37e7175dc5c01ee7ee0e654c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 10:55:56 +0000

    fix(worker): restructure view templates

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa6297f7744f4479823de3936df8a2d8 = () => {
-  return '2025-06-14T00:18:41Z';
+const node_0272f291e33b4764bcf0645153a72f19 = () => {
+  return '2025-06-30T10:55:56Z';
 };
