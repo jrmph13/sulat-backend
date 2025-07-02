1121dcdfa322e8b2b8f4c6e01e0e11f4aa150cf7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 05:18:53 +0000

    test(deps): fix middleware routes

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c9a0ca2d47040208e9fa69075f912b3 = () => {
-  return '2025-06-02T07:32:55Z';
+const node_1eb8fb7783224efdb14cf0a68c1c3f32 = () => {
+  return '2025-07-02T05:18:53Z';
 };
