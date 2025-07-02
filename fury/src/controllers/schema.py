1317d9c94bfa992d136bd0bd08b366789fd45f04
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 05:59:16 +0000

    perf(api): refactor view templates

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6a6e04bb49544d7920318b162581364 = () => {
-  return '2025-06-30T05:00:28Z';
+const node_14f015701fec4600a1a35422b2e1d235 = () => {
+  return '2025-07-02T05:59:16Z';
 };
