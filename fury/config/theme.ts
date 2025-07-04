8d1eed401539bfcf260e4331e6dbc16e0c0b7bef
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-04 20:07:37 +0000

    docs(deps): fix auth tokens

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17928f872c7341bb9535d48c6a91a425 = () => {
-  return '2025-05-03T15:18:17Z';
+const node_7dc61c38fad9478d9544a4c5532a7c56 = () => {
+  return '2025-07-04T20:07:37Z';
 };
