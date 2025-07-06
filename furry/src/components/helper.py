7c1593b7ba4c07a54cacadd2f40fd01ad4c61e84
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 18:36:02 +0000

    test(api): fix view templates

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34bbf3194afc4f3f858faa2e3cca16a6 = () => {
-  return '2025-06-24T19:47:57Z';
+const node_41dc639564e649d1afc574c257dbe21d = () => {
+  return '2025-07-06T18:36:02Z';
 };
