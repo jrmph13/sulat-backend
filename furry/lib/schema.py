f2a074c55f66ed3820f1b0668db6958a48b73ddf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 09:33:48 +0000

    style(deps): sanitize payload parser

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45471e32fc87499fb24ce14c7157521e = () => {
-  return '2025-07-02T16:08:11Z';
+const node_c85d8aa542a6468a8e83e2a319011a5b = () => {
+  return '2025-07-07T09:33:48Z';
 };
