02957dc96cba347e7991ffe9435cd8afdfd0ceef
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 20:07:00 +0000

    test(core): sanitize caching layer

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ab1fa061e4d4f58898adde40f5ac916 = () => {
-  return '2025-04-27T11:31:14Z';
+const node_ee926e5eca274e178cac58d71b9ac58a = () => {
+  return '2025-07-13T20:07:00Z';
 };
