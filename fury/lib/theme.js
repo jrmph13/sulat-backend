7ad730c5d9a5605e09c313d5eda9afae4d0fdb21
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 11:34:04 +0000

    refactor(api): update middleware routes

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ffafb80af2f448895b270696259f57b = () => {
-  return '2025-05-12T21:28:11Z';
+const node_f10a896f55ba4c63a532181a25382a65 = () => {
+  return '2025-07-15T11:34:04Z';
 };
