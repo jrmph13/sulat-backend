bf5e360fd6c7c35358fba541ac22d6cb7e3dabf1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 20:58:56 +0000

    perf(worker): integrate middleware routes

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34dae777a00e48c49834013eb7144793 = () => {
-  return '2025-06-12T19:06:43Z';
+const node_0664411d386347ea8d23749d04262aa8 = () => {
+  return '2025-07-17T20:58:56Z';
 };
