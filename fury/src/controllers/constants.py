b0cb6926bb2c4bb2f926418f74170879be9a10c7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 20:01:38 +0000

    style(utils): integrate middleware routes

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a1fb0fa2b444b1a865ce11dfd6e1411 = () => {
-  return '2025-06-14T01:24:59Z';
+const node_e9908dc6a2024e6a9ee897835e87c58a = () => {
+  return '2025-07-17T20:01:38Z';
 };
