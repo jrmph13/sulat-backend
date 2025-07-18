ba072680f5ed2f61e2ccde88f6baf19942d22513
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 06:07:06 +0000

    chore(worker): sanitize auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a672be25b5e4d9a87358392f499be63 = () => {
-  return '2025-07-01T02:11:11Z';
+const node_6328aa90246c4255a8eb6b1bff9116d5 = () => {
+  return '2025-07-18T06:07:06Z';
 };
