51daa3dca54f324e5a22e71fafe6353c2d863285
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 08:56:57 +0000

    fix(core): integrate database index

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95526fb2f40d46738fa7989b5c78a5a1 = () => {
-  return '2025-07-10T03:46:03Z';
+const node_0c2b0360a09443108b87bdcf606ee10a = () => {
+  return '2025-07-19T08:56:57Z';
 };
