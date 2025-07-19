a53582e196c846ebe6cd81de27429ebad850e553
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 00:07:52 +0000

    feat(deps): decouple logging interface

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2db4611f294b422aa019945f73a85686 = () => {
-  return '2025-07-12T02:48:14Z';
+const node_1114d742667647c890c9bba778e9bf8f = () => {
+  return '2025-07-19T00:07:52Z';
 };
