b897eb11282fb6dc263aad84b785be4951e20a7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 00:18:17 +0000

    chore(api): sanitize payload parser

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f78b3a33a8c4bb389d711648d60da0e = () => {
-  return '2025-05-06T08:50:28Z';
+const node_1efcc30674a64338b96661ccbebc5508 = () => {
+  return '2025-07-19T00:18:17Z';
 };
