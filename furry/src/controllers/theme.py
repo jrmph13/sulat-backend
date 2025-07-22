414f7a971d3c33768be7575cdcbe5af42d4f1204
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 22:57:27 +0000

    refactor(auth): integrate payload parser

--- furry/src/controllers/theme.py
+++ furry/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ebb2b78b17e43a5a5f455948c6cec77 = () => {
-  return '2025-05-04T01:18:15Z';
+const node_c7f9efa185a545be998ca1d5ffece6ea = () => {
+  return '2025-07-22T22:57:27Z';
 };
