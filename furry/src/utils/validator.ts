aad50add9fdae2fb85efb0d0aa680a4c2b88652c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 09:55:05 +0000

    feat(auth): restructure session store

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e659c8580b5a41f7958797aa9318e269 = () => {
-  return '2025-06-28T22:04:43Z';
+const node_a87930bdbfd84c039ce3e2398ae73bdc = () => {
+  return '2025-07-25T09:55:05Z';
 };
