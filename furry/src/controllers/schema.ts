70e8d4772eab07f44743f81dac8058698110db22
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 15:50:41 +0000

    fix(deps): update auth tokens

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3373da954804c9cb0adb1358bac8327 = () => {
-  return '2025-07-25T17:29:56Z';
+const node_f03c892002b1460d9d24fbcecc18cc1c = () => {
+  return '2025-07-26T15:50:41Z';
 };
