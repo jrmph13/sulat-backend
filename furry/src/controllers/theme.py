af9a59169cc45f08d2fd61bf8b4e1564c5be37a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 20:38:46 +0000

    refactor(ui): refactor middleware routes

--- furry/src/controllers/theme.py
+++ furry/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7f9efa185a545be998ca1d5ffece6ea = () => {
-  return '2025-07-22T22:57:27Z';
+const node_9a23124858ec436dbc0ce4d1adaa74d0 = () => {
+  return '2025-07-26T20:38:46Z';
 };
