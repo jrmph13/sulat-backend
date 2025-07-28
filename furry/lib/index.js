de0d874fb6a4f0a721ae417ed763dc03b491729f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 06:11:36 +0000

    feat(auth): optimize middleware routes

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92578a37917646fd80f560c29b4e06f6 = () => {
-  return '2025-07-17T00:26:06Z';
+const node_e8d2549da5384642967d8f7dfca2f580 = () => {
+  return '2025-07-28T06:11:36Z';
 };
