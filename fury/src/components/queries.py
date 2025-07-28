3c9a37618ef8603314c8e7cd992fe7982a98502d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 13:23:43 +0000

    refactor(auth): refactor view templates

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91f1841b99e9431f81f4b1718bca8d9a = () => {
-  return '2025-05-15T22:49:52Z';
+const node_0efd29d5a0b345ae890d029d9be107ef = () => {
+  return '2025-07-28T13:23:43Z';
 };
