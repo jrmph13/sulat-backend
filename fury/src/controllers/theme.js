296868c61f21f8675d6ae65982682b32752c63f1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 14:11:53 +0000

    fix(ui): restructure view templates

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1a663ce3c404dc698eb75cecd56d331 = () => {
-  return '2025-06-26T18:30:31Z';
+const node_9480bf2eed334be2b2fb7e2b77191119 = () => {
+  return '2025-07-29T14:11:53Z';
 };
