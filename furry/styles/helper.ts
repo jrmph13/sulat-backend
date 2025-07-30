31e06a5b0d7af421b1b5cce3fea72999f9dc8087
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 15:54:08 +0000

    docs(worker): refactor view templates

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_48372ee5f2a24a1facb1e5a674dece05 = () => {
-  return '2025-07-28T01:43:11Z';
+const node_fdcf5a9118074f5bb8d32b299b40c8d2 = () => {
+  return '2025-07-30T15:54:08Z';
 };
