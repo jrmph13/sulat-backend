92fe3b458bb1e94c2f7ff0f8388a9ba95e740187
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 10:31:41 +0000

    style(deps): update session store

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_90ecaf02fcea468d9ea42a9c99993952 = () => {
-  return '2025-07-07T17:50:30Z';
+const node_815bdc7e42464977a8751739cbc148b1 = () => {
+  return '2025-07-30T10:31:41Z';
 };
