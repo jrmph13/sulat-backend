0c8fbfefbb16d251a51d19cc10a02c227dbaaeea
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-01 15:46:46 +0000

    docs(api): restructure middleware routes

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4882318ac95446ab55a2b3045837061 = () => {
-  return '2025-05-28T20:33:01Z';
+const node_9a1df1659dad4347988a32504e5739f1 = () => {
+  return '2025-08-01T15:46:46Z';
 };
