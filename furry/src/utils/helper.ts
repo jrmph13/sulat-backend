56119ca310a527545f0c8770d7f3d9ae82d198cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-01 19:24:42 +0000

    chore(api): sanitize database index

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ffc5246de8e4430eb533ab8bb0bee393 = () => {
-  return '2025-06-24T22:00:35Z';
+const node_6fc6d6423c0343489f7ed61db38bff9f = () => {
+  return '2025-08-01T19:24:42Z';
 };
