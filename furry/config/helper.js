ea108c178269c9dfce5a63a0a8ef922d616caaec
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 14:31:16 +0000

    style(config): restructure validation pipeline

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7701c6013013489f9b0f7c1ae2b58f30 = () => {
-  return '2025-07-15T12:59:43Z';
+const node_6ab1c4a37130493c98716313a31f121f = () => {
+  return '2025-08-04T14:31:16Z';
 };
