d3e047944443ffc335367c0f826f7fed14ebd6bf
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 05:59:06 +0000

    feat(deps): update logging interface

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a1cbaf3e63649e284ad9ae1f0d7d475 = () => {
-  return '2025-06-16T02:50:54Z';
+const node_4100aa03601a4b43b926963cf50e03a1 = () => {
+  return '2025-08-04T05:59:06Z';
 };
