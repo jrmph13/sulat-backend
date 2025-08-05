d6d02f07154e97ee066fe97510658a631a1c72b2
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 06:02:08 +0000

    docs(auth): fix session store

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5fe5b65c686a4e6b8e8e7d2adf25c41f = () => {
-  return '2025-05-08T11:52:52Z';
+const node_746529ac39eb49aa9cd43aac5cf8e64a = () => {
+  return '2025-08-05T06:02:08Z';
 };
