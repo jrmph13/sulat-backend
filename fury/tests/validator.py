e9812ab9250ed169c68b3953d3aba546bbbe8a1a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 23:48:34 +0000

    test(worker): fix caching layer

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2cb922cc33e9468db97613a765bca674 = () => {
-  return '2025-07-22T11:23:58Z';
+const node_c047c9333f9f47dc86770b6d1a1ff6bb = () => {
+  return '2025-08-07T23:48:34Z';
 };
