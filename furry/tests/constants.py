dba295e8a43b83def921136e8b3d122ae47299b3
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 05:12:22 +0000

    style(worker): optimize payload parser

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ca91806b4e94bbba64ac77518d1662f = () => {
-  return '2025-07-31T05:36:09Z';
+const node_631890920cfb4223964c7848538ed10c = () => {
+  return '2025-08-08T05:12:22Z';
 };
