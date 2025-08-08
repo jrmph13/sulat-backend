800ad3ce2912ff27aa77063a2deca1575bad2582
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 22:18:53 +0000

    perf(auth): fix middleware routes

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a033f40c750402eb32ff7626c519b25 = () => {
-  return '2025-07-29T17:38:26Z';
+const node_6a18294fb1294092b161a514d1fc8dad = () => {
+  return '2025-08-08T22:18:53Z';
 };
