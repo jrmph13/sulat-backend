05d89d1cf4ec9ccf508bf855c62c5ec58a3532c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 15:58:41 +0000

    fix(deps): enhance auth tokens

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7705bb22247e4064847325c1a66e011c = () => {
-  return '2025-07-25T13:45:50Z';
+const node_b78292f5e0164a4a98947f67f1a3a5f9 = () => {
+  return '2025-08-10T15:58:41Z';
 };
