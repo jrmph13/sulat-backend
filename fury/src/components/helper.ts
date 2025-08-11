7cda711906256fd84563d09188af23b3baf7934e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 19:05:59 +0000

    test(ui): restructure middleware routes

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b4c069701a6b48729f18cb1b050e315a = () => {
-  return '2025-07-14T22:55:17Z';
+const node_5feb1faf99124fa7b3e7190fc90df2c1 = () => {
+  return '2025-08-11T19:05:59Z';
 };
