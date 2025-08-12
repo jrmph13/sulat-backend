674c68d2c39d400a4c5b0fd749b15b87dcc13031
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 04:30:41 +0000

    test(api): restructure logging interface

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a5896dd8f29403093186127c9b18b10 = () => {
-  return '2025-07-29T13:35:43Z';
+const node_78cb7528fcb44ed88ec4116e3cd5c81d = () => {
+  return '2025-08-12T04:30:41Z';
 };
