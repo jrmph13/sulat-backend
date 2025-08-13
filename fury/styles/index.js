506e83a8fd035517da896e26b66428ee841ad78e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-13 12:08:41 +0000

    perf(api): update database index

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7bd5c136cd9a45729cd4b700dfc8bd1d = () => {
-  return '2025-08-03T19:23:20Z';
+const node_c6053678104c4b6f98114989c89a3ad8 = () => {
+  return '2025-08-13T12:08:41Z';
 };
