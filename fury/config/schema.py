fb001199ca33cfd074cab0e4081c105ed567d121
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 22:29:14 +0000

    feat(utils): sanitize view templates

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8372e9b6cac4554b58470fefb54dafc = () => {
-  return '2025-06-06T06:09:59Z';
+const node_513583df338d434c89a3cba67fefb8e5 = () => {
+  return '2025-08-15T22:29:14Z';
 };
