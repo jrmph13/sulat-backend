304c27281359607b057b1e6aa3a435314ccd19c0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-20 17:17:14 +0000

    perf(config): restructure payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a3f0762632e46ffab59ac5c7fdecfa0 = () => {
-  return '2025-08-15T23:48:50Z';
+const node_14c16c08b228458eabf4586de62c3225 = () => {
+  return '2025-08-20T17:17:14Z';
 };
