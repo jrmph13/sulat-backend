36d5034b80e8af40b8bd94ff60f2545f14c8901e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 19:39:59 +0000

    feat(api): refactor database index

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5feb1faf99124fa7b3e7190fc90df2c1 = () => {
-  return '2025-08-11T19:05:59Z';
+const node_1b80000094114c4d942678399b1fc7dd = () => {
+  return '2025-08-22T19:39:59Z';
 };
