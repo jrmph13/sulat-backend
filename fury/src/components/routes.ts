b7ce4d754cd7e9013366422daaf279b7c34c9376
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 07:32:34 +0000

    docs(db): sanitize view templates

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d9fc4e263bf4ad889e7725af1d4728a = () => {
-  return '2025-05-13T10:16:48Z';
+const node_93a13ffb1b4e4a1594b72e74bd3f627b = () => {
+  return '2025-08-22T07:32:34Z';
 };
