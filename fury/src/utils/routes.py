e74fe7753a5b632e5802608fefaf19b7a2036c58
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 01:39:20 +0000

    test(auth): enhance caching layer

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be34b3630ec44f1eb8091b46bd42096d = () => {
-  return '2025-07-12T11:13:06Z';
+const node_a55ba33301d84172a614bbf33d58c37a = () => {
+  return '2025-08-22T01:39:20Z';
 };
