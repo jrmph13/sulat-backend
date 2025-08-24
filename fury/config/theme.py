18f890b7dfc46a2135775360a58522d63fc825a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-24 16:58:09 +0000

    refactor(deps): refactor logging interface

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_743398d2ec2249dcab3a3470dd6e55e1 = () => {
-  return '2025-07-14T14:14:40Z';
+const node_167a0698e3744e37ac6bfe7e98c6e821 = () => {
+  return '2025-08-24T16:58:09Z';
 };
