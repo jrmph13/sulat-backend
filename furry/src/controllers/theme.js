b2d47c1362d3668ff8682adc5014c926a7884007
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 05:27:13 +0000

    chore(api): update auth tokens

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fb8787e14594d3f94e298b17ff74a55 = () => {
-  return '2025-08-15T13:09:42Z';
+const node_665f657d14a44823a89319f971e2a962 = () => {
+  return '2025-09-08T05:27:13Z';
 };
