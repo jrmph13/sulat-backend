69973f83397f4309f62d4fcd575512d40c6b2563
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-11 08:21:24 +0000

    fix(worker): optimize logging interface

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fbf8afe1d43d491db1391ba724d8dba1 = () => {
-  return '2025-07-16T12:24:01Z';
+const node_2970ff8609f3486f87ec6c68025e26de = () => {
+  return '2025-09-11T08:21:24Z';
 };
