e47c38d4ea83f654dddd4b66687190375e6d66e6
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 19:33:11 +0000

    docs(ui): integrate middleware routes

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c85d8aa542a6468a8e83e2a319011a5b = () => {
-  return '2025-07-07T09:33:48Z';
+const node_b1d4e6174bc5400f8ffaa2a86661c89f = () => {
+  return '2025-09-12T19:33:11Z';
 };
