c16dcda35379ec4260028328f34fdde36dbdb179
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-15 20:32:09 +0000

    chore(utils): fix view templates

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1d4e6174bc5400f8ffaa2a86661c89f = () => {
-  return '2025-09-12T19:33:11Z';
+const node_3a618ce32f704aee9a1ed3768e9b5d32 = () => {
+  return '2025-09-15T20:32:09Z';
 };
