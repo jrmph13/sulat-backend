f72560dd397e64776890bb4b5a6a6aa75011e0ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-18 14:00:27 +0000

    fix(db): integrate view templates

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04837f7100d345e8bbfa7b2120e72610 = () => {
-  return '2025-07-01T23:04:04Z';
+const node_ec49a26612e04b0fb2555230630c9f3e = () => {
+  return '2025-09-18T14:00:27Z';
 };
