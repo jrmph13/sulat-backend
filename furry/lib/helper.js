77aa251fcdffce59f41e011579bc401822d52894
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 08:43:18 +0000

    test(auth): sanitize middleware routes

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0664411d386347ea8d23749d04262aa8 = () => {
-  return '2025-07-17T20:58:56Z';
+const node_dc82c80f1e5d4085ad13a49179f7f1a9 = () => {
+  return '2025-09-27T08:43:18Z';
 };
