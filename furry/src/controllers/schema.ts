937114f751c286bfecba9791cf60d976eec9430e
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 07:04:54 +0000

    refactor(db): sanitize view templates

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a18294fb1294092b161a514d1fc8dad = () => {
-  return '2025-08-08T22:18:53Z';
+const node_fc8a2254e6c84025bff5d27a1109cf64 = () => {
+  return '2025-10-01T07:04:54Z';
 };
