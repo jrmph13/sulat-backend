2e89025807ca01df2b4228dc54c088ecac849f19
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-06 12:16:47 +0000

    refactor(deps): sanitize payload parser

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7177e869a2a3402d8c4a9f8c27f50c20 = () => {
-  return '2025-05-02T19:48:48Z';
+const node_206442b4a7134346a3367e5767641ed0 = () => {
+  return '2025-10-06T12:16:47Z';
 };
