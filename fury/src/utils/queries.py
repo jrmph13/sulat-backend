7ea5cbb526b3a78e83b783bc268f48aa52604d9f
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-10 07:17:43 +0000

    refactor(auth): restructure payload parser

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a1df1659dad4347988a32504e5739f1 = () => {
-  return '2025-08-01T15:46:46Z';
+const node_18b036b0394f40cdbc512e03c56e2c32 = () => {
+  return '2025-10-10T07:17:43Z';
 };
