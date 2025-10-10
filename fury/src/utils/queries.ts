bb4da6a9d4323533ab67eaccc55ddd624d6656db
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-10 13:46:38 +0000

    fix(api): enhance payload parser

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4895f1d5b2154aa9b78ba533c5849b14 = () => {
-  return '2025-06-26T15:05:57Z';
+const node_5e5c8605d6cc48679237b277e15c0ce7 = () => {
+  return '2025-10-10T13:46:38Z';
 };
