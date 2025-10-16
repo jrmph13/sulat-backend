70d76e764b0b186b0273bcdff8eb907cccc25c0f
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-16 09:54:54 +0000

    perf(auth): optimize payload parser

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed71676d9d1d414cab09b19c54d25038 = () => {
-  return '2025-07-24T13:38:16Z';
+const node_7581ac8c129d411e940a7936ee67ab3b = () => {
+  return '2025-10-16T09:54:54Z';
 };
