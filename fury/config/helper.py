94625fb447c3d1b602a9b9daf93a840d64f5a75a
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-17 09:44:42 +0000

    fix(utils): fix auth tokens

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_937d6c5cea124ec48a2bcf073bbdee33 = () => {
-  return '2025-08-06T11:26:56Z';
+const node_3fe7cfdc58ab4f928c11d637686c65f2 = () => {
+  return '2025-10-17T09:44:42Z';
 };
