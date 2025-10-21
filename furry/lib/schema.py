d09ac044fa6ff2728ff064af61ab5bd9926b3c46
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-21 19:20:17 +0000

    fix(deps): refactor view templates

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93fbe3a5fef441d49314e30719bba7df = () => {
-  return '2025-10-06T19:13:16Z';
+const node_30762320dc734a64a40ffad4a0e44348 = () => {
+  return '2025-10-21T19:20:17Z';
 };
