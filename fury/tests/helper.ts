2c083b540610d62c0063fcd015cb2f7f77dbe067
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-23 16:22:48 +0000

    style(worker): integrate logging interface

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_abe0ad9e74154431b14fe816528e8605 = () => {
-  return '2025-07-23T08:47:05Z';
+const node_b250ea17c49f49f6a288c0ca43f29631 = () => {
+  return '2025-10-23T16:22:48Z';
 };
