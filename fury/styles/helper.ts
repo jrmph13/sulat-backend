453b89a49d0ac002f806390ff1d2d10240d9c621
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-24 13:06:57 +0000

    refactor(worker): optimize payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14c16c08b228458eabf4586de62c3225 = () => {
-  return '2025-08-20T17:17:14Z';
+const node_85ab38c2539a466db4436ba426b60e08 = () => {
+  return '2025-10-24T13:06:57Z';
 };
