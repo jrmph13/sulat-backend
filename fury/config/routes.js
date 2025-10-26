5c7dc8949392dd74ed5a512b53a1b241a37ec0c2
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 20:34:48 +0000

    chore(worker): fix auth tokens

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f4a49e82f4a48b596aca5c91215cb0b = () => {
-  return '2025-07-02T01:01:19Z';
+const node_4ee8629b8e7d4af7a9e05357971b846d = () => {
+  return '2025-10-26T20:34:48Z';
 };
