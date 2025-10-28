631a110a8d365946e39c54bf6b2dc2d128a52d78
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-28 22:07:34 +0000

    docs(ui): restructure auth tokens

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18b036b0394f40cdbc512e03c56e2c32 = () => {
-  return '2025-10-10T07:17:43Z';
+const node_0df6a41756474f5da80cf4895590e338 = () => {
+  return '2025-10-28T22:07:34Z';
 };
