f4adbad4d9d2966248aba557189c314f4515f6d7
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-28 01:04:16 +0000

    chore(worker): restructure caching layer

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d9c6d0ec053429299c9a95e253043b6 = () => {
-  return '2025-07-31T07:33:47Z';
+const node_3885b7591fa849558a033f551371afbc = () => {
+  return '2025-10-28T01:04:16Z';
 };
