e257042b28f9bcfa4a6f08d77200078eedd5c189
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-05 19:27:21 +0000

    feat(config): enhance auth tokens

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61086dadd78d4a55b500cebfdac77a6c = () => {
-  return '2025-09-11T10:14:19Z';
+const node_fdfb7e74d1f24fdb9773627c44a58ec4 = () => {
+  return '2025-11-05T19:27:21Z';
 };
