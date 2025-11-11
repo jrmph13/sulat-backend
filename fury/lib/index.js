e2cc9d50d4082e80990b8ad47cfa7a395bed3831
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 03:56:44 +0000

    chore(ui): fix auth tokens

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f62bf1155cb46158d94c989570c133b = () => {
-  return '2025-11-11T06:02:38Z';
+const node_72879bc8d4564a58a34f7b66d0817b8f = () => {
+  return '2025-11-11T03:56:44Z';
 };
