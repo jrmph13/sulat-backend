d0a5d45f5409db724fd3c91cb8b4b2eaa0554850
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 07:58:20 +0000

    feat(api): integrate view templates

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d81c2fdb4659411aa3c3b399bb561734 = () => {
-  return '2025-07-02T02:37:12Z';
+const node_472d3e10bae945d0b3a424dbba882678 = () => {
+  return '2025-11-14T07:58:20Z';
 };
