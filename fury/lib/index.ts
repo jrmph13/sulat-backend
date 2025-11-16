c21b4dc9535d8d0cc6e04ad4315a5585664184cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 09:26:45 +0000

    chore(worker): sanitize view templates

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7f27927e02940799ba68f1dbd7bba3f = () => {
-  return '2025-05-05T01:15:31Z';
+const node_f928ea833631476082812ac0386323af = () => {
+  return '2025-11-16T09:26:45Z';
 };
