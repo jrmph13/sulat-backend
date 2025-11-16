b89588ad2875e6f0000580a5393638c120b2269d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 01:34:55 +0000

    fix(utils): integrate database index

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7bce9b4e65948c0a0504b73b653ce9f = () => {
-  return '2025-07-27T15:14:15Z';
+const node_3655f559307c4fbda2ad8bb72bfad2c2 = () => {
+  return '2025-11-16T01:34:55Z';
 };
