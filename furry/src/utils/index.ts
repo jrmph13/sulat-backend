e35ce18751afc69adcc2f5e2d2649619763cad99
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 12:25:14 +0000

    perf(ui): implement payload parser

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_957442e528e443fabdeca5650a78377f = () => {
-  return '2025-10-06T06:22:05Z';
+const node_d900fefb11ad4e6fbcbef8eff62e8d8a = () => {
+  return '2025-11-18T12:25:14Z';
 };
