9e2938bcde7b43a5bf0d76966744b45a75970bda
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 13:43:04 +0000

    fix(core): decouple database index

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e8b0cfb829bb4e829a32d0d59e142989 = () => {
-  return '2025-06-30T05:55:56Z';
+const node_45dd9615e9cd44a09cce02d7e32c709e = () => {
+  return '2025-11-26T13:43:04Z';
 };
