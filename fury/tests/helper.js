34ba89f30f5ab1d2ee2271102270735dd25771ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-28 11:46:16 +0000

    chore(api): restructure caching layer

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12d84a65f7ba440d8f2cef6f4bf3ee87 = () => {
-  return '2025-10-27T15:33:28Z';
+const node_5d504593e5604da2a4c89c935ed20f14 = () => {
+  return '2025-11-28T11:46:16Z';
 };
