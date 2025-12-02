89f812fc4d48cfd376a8f027224ef13e6151793e
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-02 13:12:15 +0000

    perf(auth): restructure payload parser

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a31c4e81a62842cd8ca0303776294a6c = () => {
-  return '2025-06-15T13:51:41Z';
+const node_9c7dc8046c844bc0b8fa0df8a4c60df2 = () => {
+  return '2025-12-02T13:12:15Z';
 };
