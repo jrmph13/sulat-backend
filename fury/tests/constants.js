7a190b8570ce83e4d4585908f1ea05d1d5f06ce5
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 19:37:03 +0000

    fix(ui): optimize validation pipeline

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84a289d5b27b4663b9a6e1afe18d3000 = () => {
-  return '2025-08-02T21:04:22Z';
+const node_46479bf9d53143df8ff4c6018c25384d = () => {
+  return '2025-12-04T19:37:03Z';
 };
