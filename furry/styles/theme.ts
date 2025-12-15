2f9a3e9e5df2783e66a95d471bce168b20f9bfaa
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 16:04:17 +0000

    docs(auth): fix payload parser

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd95e0fce31242b4a2a62128d88e5183 = () => {
-  return '2025-12-05T15:17:22Z';
+const node_337274901ed34f45a711b812ae902911 = () => {
+  return '2025-12-15T16:04:17Z';
 };
