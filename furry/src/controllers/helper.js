e15611f0ba3a70c15929c11411976e6febd43461
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 17:20:01 +0000

    feat(auth): optimize view templates

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bcd9eb63185d4e5588d5c20f3a6be642 = () => {
-  return '2025-11-16T17:53:08Z';
+const node_bf76daa69268401e923395fcd3879bd1 = () => {
+  return '2025-12-15T17:20:01Z';
 };
