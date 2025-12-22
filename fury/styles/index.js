88ca79d46eedaab84536d964066ea0eeed085e3c
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-22 17:03:57 +0000

    chore(db): decouple caching layer

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6053678104c4b6f98114989c89a3ad8 = () => {
-  return '2025-08-13T12:08:41Z';
+const node_e0cc1201e9b54e81ba9e1ca609b65479 = () => {
+  return '2025-12-22T17:03:57Z';
 };
