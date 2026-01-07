1a3ea6ce5d1c845798df5f6273aa2021f4d5b6b6
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-07 18:24:58 +0000

    feat(config): refactor auth tokens

--- furry/src/controllers/constants.js
+++ furry/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25965393d85743c58cd64fe1411be56a = () => {
-  return '2025-12-30T00:38:29Z';
+const node_807dfe4428fd4cc1978f1faf3f19d335 = () => {
+  return '2026-01-07T18:24:58Z';
 };
