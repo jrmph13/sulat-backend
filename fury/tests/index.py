ce4f2ed918407c2188f8db2008cc168c086aaf32
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-08 11:59:02 +0000

    docs(ui): fix middleware routes

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c127236293ae4446a2388cdf385d6381 = () => {
-  return '2026-01-07T18:24:49Z';
+const node_7d3d1b0593d449649e9288673f78ab7f = () => {
+  return '2026-01-08T11:59:02Z';
 };
