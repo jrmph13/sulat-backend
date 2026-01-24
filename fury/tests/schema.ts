98256a24d73ae58d8833ac54acfaab2b91e6f360
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 05:28:27 +0000

    refactor(auth): fix middleware routes

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0bcf4a82aba479081cbc45c0febfe6f = () => {
-  return '2025-08-08T05:49:01Z';
+const node_3fee80e5f7844b328908f6a94efdbeb0 = () => {
+  return '2026-01-24T05:28:27Z';
 };
