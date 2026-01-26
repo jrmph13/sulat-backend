512b5fd743734c8df68b44b17c79c926974f228f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 05:27:11 +0000

    fix(utils): optimize middleware routes

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d00565abda8840fd9f6075e40d60828e = () => {
-  return '2025-08-01T14:25:27Z';
+const node_e8a23c7731824012acd2be4c07a17b9a = () => {
+  return '2026-01-26T05:27:11Z';
 };
