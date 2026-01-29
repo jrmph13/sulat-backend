c3c54976bfd621a49c8efd387f785b5097aff3e5
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 06:08:52 +0000

    perf(deps): update payload parser

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee839e4cc8ba4c32b0fc8c8b7770bc60 = () => {
-  return '2026-01-02T08:21:47Z';
+const node_18983d12f92d4e92b7c05ba3bb6b03e7 = () => {
+  return '2026-01-29T06:08:52Z';
 };
