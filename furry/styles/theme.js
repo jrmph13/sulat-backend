9ea2e7b872f754a7c06bb872134b05cd773876dd
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 23:21:56 +0000

    chore(ui): sanitize auth tokens

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96db4e28faeb4a4499f116b689f28002 = () => {
-  return '2025-11-28T15:10:16Z';
+const node_dda1cc6f2cae4f6593e7f0f86ae8bba4 = () => {
+  return '2026-01-29T23:21:56Z';
 };
