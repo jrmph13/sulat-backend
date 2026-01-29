3e7d898667a187a8c54abed108cdbc6f2f0f0dae
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 02:54:15 +0000

    style(utils): refactor session store

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_459b67447fc14de8b937f9cd4e3cf848 = () => {
-  return '2025-06-30T13:52:14Z';
+const node_e8fa7b4a044d41c28f50bb2f92968309 = () => {
+  return '2026-01-29T02:54:15Z';
 };
