e618e1243364d762af9df407379799e6db89ccb8
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 21:13:45 +0000

    perf(deps): restructure middleware routes

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5c87a2bef19442d9e7bc117e4b2fec8 = () => {
-  return '2025-10-13T23:45:17Z';
+const node_9d3a169d3e0b4be98a02eafd83cdf120 = () => {
+  return '2026-02-02T21:13:45Z';
 };
