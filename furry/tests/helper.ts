34c4d54d755b01fd3d8d4988c49e5d45154a68a1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 14:18:48 +0000

    fix(core): integrate view templates

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3cadcb304ea6402e8dfc68f53d8f7e20 = () => {
-  return '2026-01-24T19:25:36Z';
+const node_6cf85691ef874785b6cb4367f1594d37 = () => {
+  return '2026-02-02T14:18:48Z';
 };
