1710c3d8200b6aea8c98641b2b9df70c24ddb0c0
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 18:00:19 +0000

    refactor(api): decouple auth tokens

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5bcd519b9f3646ee88701c0c8a361e83 = () => {
-  return '2026-01-01T18:17:06Z';
+const node_ef90f8127bb24ccf96c8072384c27b6d = () => {
+  return '2026-02-03T18:00:19Z';
 };
