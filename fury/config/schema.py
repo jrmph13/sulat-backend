c2d6335a1b77696d0f2fb335a0a2d65ae2dfacf7
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 00:55:41 +0000

    chore(worker): integrate session store

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_513583df338d434c89a3cba67fefb8e5 = () => {
-  return '2025-08-15T22:29:14Z';
+const node_a158991f07014136ba59c55407e95eb7 = () => {
+  return '2026-02-04T00:55:41Z';
 };
