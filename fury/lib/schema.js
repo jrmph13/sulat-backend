5aa28bac779a1b968fb0c275be53f597e7edd2cf
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 11:17:38 +0000

    test(utils): integrate view templates

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b588993d56e4691b9b452b02100bf50 = () => {
-  return '2025-11-04T07:16:27Z';
+const node_62119f107f4e44a489dcc04ba2c39bd6 = () => {
+  return '2026-02-06T11:17:38Z';
 };
