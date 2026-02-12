deb2bb4374252d3ef3c8a41b88da84660d7e04db
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 12:15:55 +0000

    test(auth): restructure auth tokens

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18983d12f92d4e92b7c05ba3bb6b03e7 = () => {
-  return '2026-01-29T06:08:52Z';
+const node_2af282bed7c745d294e09afbe8b05597 = () => {
+  return '2026-02-12T12:15:55Z';
 };
