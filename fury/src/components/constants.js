c41f2a5d9d3380f3b451fc5c16ffdb6346cd07bb
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 21:35:32 +0000

    test(ui): update middleware routes

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9040dce2893243068c6cfd011edc3f53 = () => {
-  return '2026-02-09T03:31:26Z';
+const node_86019bdeaaf841d6be85265ac8c2713a = () => {
+  return '2026-02-16T21:35:32Z';
 };
