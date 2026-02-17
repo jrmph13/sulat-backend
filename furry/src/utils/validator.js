96f8ef6a6df438514eaaf37fc56fc61b75317ce1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 14:23:10 +0000

    style(auth): sanitize middleware routes

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae53744096594d91a8fd7c8614d914e0 = () => {
-  return '2025-12-10T14:04:31Z';
+const node_15480fe76d844430b0ee947537985ffa = () => {
+  return '2026-02-17T14:23:10Z';
 };
