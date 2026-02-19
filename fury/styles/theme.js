56e480d5ab299c3d25a697f03eeb7236f84f14df
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 05:30:22 +0000

    perf(api): decouple caching layer

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9777096f4d440d09e7ce7986ff27e2a = () => {
-  return '2026-02-12T17:31:19Z';
+const node_17c073da276340deb81da60fea128ddd = () => {
+  return '2026-02-19T05:30:22Z';
 };
