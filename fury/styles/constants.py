e3f7c40058b106caf602d33f8480bea7492b7685
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 16:52:54 +0000

    perf(core): update payload parser

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d764f3d84bbe4a35a14412234f0d6894 = () => {
-  return '2026-02-12T03:23:49Z';
+const node_a6b168b661474976b9575505e3781309 = () => {
+  return '2026-02-22T16:52:54Z';
 };
