272e99e9b4d33ec4112d939c7d1b249e5e63d437
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 01:21:43 +0000

    fix(auth): update middleware routes

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f6214ce39624ac5abdd46a40b627dd0 = () => {
-  return '2026-01-01T01:27:07Z';
+const node_ef4f8a1bf7694793a5d45a0d2dc0b9ce = () => {
+  return '2026-02-22T01:21:43Z';
 };
