da6cafac1bad5bce6c7157f8a2520569abb56dc0
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 20:31:33 +0000

    perf(core): sanitize auth tokens

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eca43464863c4a59baca7b008909995e = () => {
-  return '2026-02-04T12:54:17Z';
+const node_869736062c514f38b497f41c363b5473 = () => {
+  return '2026-02-22T20:31:33Z';
 };
