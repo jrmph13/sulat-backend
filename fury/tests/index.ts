ae89bf1e3e242535cd8476f22ff52863e14b04ba
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 00:51:18 +0000

    chore(api): integrate payload parser

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a8bede887174124a93d9b8bca072b2f = () => {
-  return '2026-02-06T02:29:07Z';
+const node_502825665ad54e64bf8961c166961b17 = () => {
+  return '2026-02-25T00:51:18Z';
 };
