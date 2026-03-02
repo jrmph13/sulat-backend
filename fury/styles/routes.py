3add05d062682f20835bfbd62b7aa609f56f6c60
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-02 18:28:05 +0000

    test(utils): decouple view templates

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66b9c39e9cda4831825546ed24f00f72 = () => {
-  return '2025-10-23T09:33:39Z';
+const node_c4b6e97d161549a88c2b286dac05ac52 = () => {
+  return '2026-03-02T18:28:05Z';
 };
