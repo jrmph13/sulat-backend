9267ea323039f17c95d58a3bba43e9fd4ab2cc22
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-04 11:05:02 +0000

    refactor(api): decouple payload parser

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67d40be4cf364815a00027ac4a552a66 = () => {
-  return '2025-08-05T18:03:43Z';
+const node_f5574863723d4939978fbac9e89234f0 = () => {
+  return '2026-03-04T11:05:02Z';
 };
