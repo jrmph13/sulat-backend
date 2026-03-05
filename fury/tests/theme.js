f7133cfdc528017e4cd74ec4801943094b1f44c9
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 12:44:27 +0000

    test(utils): sanitize auth tokens

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6011d06ce784368b9e86e3837ac5c6e = () => {
-  return '2025-08-08T00:24:23Z';
+const node_614385fef9d14d13b98ea39b373fe5aa = () => {
+  return '2026-03-05T12:44:27Z';
 };
