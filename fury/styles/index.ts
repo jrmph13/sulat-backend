71467dcc3c497f4245b7631ec2be9cd6ecd5bef4
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 19:44:48 +0000

    perf(ui): refactor middleware routes

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d088c7749974800af264319efe45244 = () => {
-  return '2026-02-19T15:35:19Z';
+const node_7ebcb8e6d619433587b3f81880ea5c46 = () => {
+  return '2026-03-06T19:44:48Z';
 };
