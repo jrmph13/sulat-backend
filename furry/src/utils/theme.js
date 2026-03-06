9e38ffa1c274428267877a8213ac7e48b498b7ae
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 03:15:12 +0000

    feat(core): restructure caching layer

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62e2faf5c707485ba6d0750f4fc6b53f = () => {
-  return '2026-03-06T16:07:16Z';
+const node_ab48bb4f824245c1bb9e1b931fafe669 = () => {
+  return '2026-03-06T03:15:12Z';
 };
