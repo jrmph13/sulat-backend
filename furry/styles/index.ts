d97fd27c3841b3aa41f0efbd86499abb101412c0
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 18:15:13 +0000

    style(ui): restructure middleware routes

--- furry/styles/index.ts
+++ furry/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4fa1fe0f5d554ab29d8d4c3f2cc89167 = () => {
-  return '2026-02-22T04:23:57Z';
+const node_50ba56b3527740d6bb75ddd6a2b7b204 = () => {
+  return '2026-03-10T18:15:13Z';
 };
