307499ea866d497f86f33589cd7d9f3abdb57d23
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 02:20:25 +0000

    chore(auth): restructure validation pipeline

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d474e3b4e3c14927a6dd9fb7eea598ca = () => {
-  return '2026-03-10T03:18:46Z';
+const node_12cbb86577414d35a942d505e30a926b = () => {
+  return '2026-03-10T02:20:25Z';
 };
