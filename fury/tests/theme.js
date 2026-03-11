dfc8ca46fe9b0cf6b444fa255f0bde260847f71e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 22:57:56 +0000

    docs(auth): refactor auth tokens

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_614385fef9d14d13b98ea39b373fe5aa = () => {
-  return '2026-03-05T12:44:27Z';
+const node_d0f6185c93b14b86a1293809d383e2a1 = () => {
+  return '2026-03-11T22:57:56Z';
 };
