4624bb9302b0add116d8e3bfeb17102d8ca4628a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 12:19:46 +0000

    fix(ui): update payload parser

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddf88ecc3e9b43faa6b3e07f580601a7 = () => {
-  return '2026-02-25T06:26:38Z';
+const node_abca24c47761438283ac1d18c4f86ddb = () => {
+  return '2026-03-12T12:19:46Z';
 };
