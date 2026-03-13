23c82d2432cb2ce287537e8ba13ec6192a16430b
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 20:11:12 +0000

    fix(auth): implement auth tokens

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cb107bc8caf4b139f586c8e39f73c20 = () => {
-  return '2026-03-12T07:06:09Z';
+const node_a17862c26f5c4024b23c389c92029a2c = () => {
+  return '2026-03-13T20:11:12Z';
 };
