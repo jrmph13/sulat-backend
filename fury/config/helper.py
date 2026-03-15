5d43cd11244e4ae28372f8674562125a2e1d2a50
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 13:03:16 +0000

    refactor(config): integrate auth tokens

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db9d606a636d49f3997b94d4b09986d5 = () => {
-  return '2026-02-22T22:17:19Z';
+const node_b01a751d07234705b79ea57b66c99182 = () => {
+  return '2026-03-15T13:03:16Z';
 };
