faace0f7423f895bbbcd8754dec6d6b63e1106f6
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 04:38:36 +0000

    fix(ui): integrate view templates

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_169922e0550d49198fc89732792c9e09 = () => {
-  return '2026-03-12T05:20:58Z';
+const node_28a3212e14b0460295f0e36c645f07a8 = () => {
+  return '2026-03-15T04:38:36Z';
 };
