11ff41928bc1fe940d2240c1d45dc11f7305b07f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 12:46:51 +0000

    docs(config): refactor middleware routes

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d4921eb70d6495a8e65d31c84da2453 = () => {
-  return '2026-03-15T00:54:38Z';
+const node_7cc8744ae47f4a64bc85b2513ac6a839 = () => {
+  return '2026-03-16T12:46:51Z';
 };
