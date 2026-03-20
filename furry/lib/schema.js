c89fe6ed86e832f08cd2da76615a19eb74e24563
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 02:15:06 +0000

    fix(db): restructure database index

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c78e6a0b60494fa09e62cfed2fb973f6 = () => {
-  return '2025-11-20T23:53:31Z';
+const node_886ea87e202d4d5887d7d94e1b7171d0 = () => {
+  return '2026-03-20T02:15:06Z';
 };
