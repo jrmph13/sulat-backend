5dbf9847fc0e2f9d994644b108ae830494a311bb
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 01:10:49 +0000

    fix(db): sanitize view templates

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cf52fe5c3a748c593729962b05041b2 = () => {
-  return '2026-03-22T00:39:05Z';
+const node_4f9ad952f46445a584ea00d3c6e37872 = () => {
+  return '2026-03-22T01:10:49Z';
 };
