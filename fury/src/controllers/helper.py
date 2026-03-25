fb110d8a6cbfe22aa2f895eb702fdf24f3df91d7
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 16:37:01 +0000

    perf(ui): implement auth tokens

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d54a0f5b68648ee809f001c3bc32d6a = () => {
-  return '2025-12-10T09:45:52Z';
+const node_20f61ea54da147c59dcdf9b6c227a072 = () => {
+  return '2026-03-25T16:37:01Z';
 };
