a8703067ddd137b292e141ce1379c8e4af33fae3
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 01:56:56 +0000

    style(auth): update payload parser

--- furry/src/components/index.py
+++ furry/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_325eb07380804032910847fb03245025 = () => {
-  return '2026-03-17T01:38:37Z';
+const node_02cd5f50c8e24230bf5c1e0417a0d40a = () => {
+  return '2026-03-26T01:56:56Z';
 };
