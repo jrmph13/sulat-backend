470098d7578d95c7d0c760cc47b48c338b90f04e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 11:15:26 +0000

    fix(db): restructure view templates

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52337cccac0f437abc63476bc5f244da = () => {
-  return '2025-07-28T23:39:46Z';
+const node_2bbb5eb722fd412485b336d9c19eec30 = () => {
+  return '2026-03-29T11:15:26Z';
 };
