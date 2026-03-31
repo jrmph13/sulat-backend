f6f1196ae1e880fcfaaa286606f31d40b67501a3
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 20:05:01 +0000

    fix(core): restructure view templates

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f402fd154b49476999bccdfddf79c83f = () => {
-  return '2026-03-20T03:10:46Z';
+const node_424ff050b7f24529abdccd0436246bf0 = () => {
+  return '2026-03-31T20:05:01Z';
 };
