4b860dc55131d182a283d1422faaa3af829423da
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 18:13:45 +0000

    test(auth): implement session store

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b9202dafee434bd9ba82b9d614ea5f4a = () => {
-  return '2026-03-14T09:05:58Z';
+const node_f121b932e6464c338f51229ae4210983 = () => {
+  return '2026-04-01T18:13:45Z';
 };
