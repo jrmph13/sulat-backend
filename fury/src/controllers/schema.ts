bf0be9809385dc4b64b9a34581456857aaa6f2cb
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 19:14:20 +0000

    fix(utils): decouple session store

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23dae477222a4827b6bb70ccadd79bb2 = () => {
-  return '2026-02-19T11:21:29Z';
+const node_1c79593e3d6549afaad7c2974bd50157 = () => {
+  return '2026-04-01T19:14:20Z';
 };
