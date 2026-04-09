2613dedb0b3b125f4bcf7d9c4545272e33a099ae
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 17:57:08 +0000

    feat(worker): optimize auth tokens

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_167938359717446cb0fde51ff3dcbc52 = () => {
-  return '2026-03-18T07:04:41Z';
+const node_675c45c30b024fed9de24d6e3f70ff18 = () => {
+  return '2026-04-09T17:57:08Z';
 };
