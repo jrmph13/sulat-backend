c44b4a3e5c8ae94feee53350a79af016cc956186
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 03:17:55 +0000

    fix(auth): optimize database index

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4d99e1d6e27440199518ccac9e11c87 = () => {
-  return '2026-01-27T07:48:08Z';
+const node_2554d00d6ba042b8a34b3cda95cfcbd3 = () => {
+  return '2026-04-14T03:17:55Z';
 };
