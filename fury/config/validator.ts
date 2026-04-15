29fbec51ec304e37a16392b27b62b22404995aa2
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 18:36:29 +0000

    chore(worker): fix database index

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d8ca8f0656944efbf263e16cb065023 = () => {
-  return '2026-04-15T22:22:09Z';
+const node_48362a118ecd4a609ce4f13d6b6a0cc2 = () => {
+  return '2026-04-15T18:36:29Z';
 };
