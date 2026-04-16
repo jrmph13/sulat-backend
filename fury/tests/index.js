2a216c5416ddf3551fbd097750cc567e6f1e8108
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 12:29:59 +0000

    chore(db): refactor session store

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_695fd8a7eed74a44bb5417015cfdbf23 = () => {
-  return '2026-02-22T18:42:28Z';
+const node_707146d6735e4693ba411ed0ebaa0b50 = () => {
+  return '2026-04-16T12:29:59Z';
 };
