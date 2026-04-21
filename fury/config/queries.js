550d56bb567bac6d62eba0cd044f1d4c32d75500
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 12:40:50 +0000

    perf(config): update view templates

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d87cc97b8964ac4b44b8a2a2191d90d = () => {
-  return '2026-02-23T13:53:15Z';
+const node_564feef2b08448d0b67501dc699146cd = () => {
+  return '2026-04-21T12:40:50Z';
 };
