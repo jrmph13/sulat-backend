ec1299dba1f13cd7c5914fdd1d444f012d5c9303
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 16:46:15 +0000

    chore(config): decouple auth tokens

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25c0d2d2d9b243d8a1e4beff1e36684b = () => {
-  return '2026-04-15T02:22:56Z';
+const node_48d8938efd9a44029e54804e8fdbee2e = () => {
+  return '2026-04-24T16:46:15Z';
 };
