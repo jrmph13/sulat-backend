1754342f4cf0005eb72ec24490dce8b4d5946914
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 08:34:04 +0000

    fix(utils): integrate auth tokens

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0f6185c93b14b86a1293809d383e2a1 = () => {
-  return '2026-03-11T22:57:56Z';
+const node_3a34cedcdc044cb29c7266c6b2157ded = () => {
+  return '2026-04-27T08:34:04Z';
 };
