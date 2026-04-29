7d77bd7667a41a7967e0ff320574966eabb279b2
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-29 13:25:39 +0000

    feat(utils): refactor auth tokens

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69e1c8224d6c41a7b7e6a3fe4e8d0cf3 = () => {
-  return '2026-02-25T09:56:19Z';
+const node_e3346d9fff7742d79cd898465e652dff = () => {
+  return '2026-04-29T13:25:39Z';
 };
