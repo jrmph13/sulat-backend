3dabc8ac872c5836e859c51e08f83a2125e9af72
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 09:16:40 +0000

    test(worker): fix auth tokens

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f0b27221d9704d0a9c686f65ef21b330 = () => {
-  return '2026-03-16T22:31:18Z';
+const node_876296f9e4fd45f0be71f1207df0e2a5 = () => {
+  return '2026-05-02T09:16:40Z';
 };
