b4ab931075da3bcf30c9af043c07be05d3ad9a3e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 04:44:55 +0000

    chore(api): integrate view templates

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20a891eb7ebd4092ac798cb02fcf06ef = () => {
-  return '2026-02-12T13:22:59Z';
+const node_e81c5f8b168f44ee86f74c77568df2fa = () => {
+  return '2026-05-02T04:44:55Z';
 };
