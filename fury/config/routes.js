9d7f4fc4ebaa8c143f5966093097f33ac8fcc7d4
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 09:24:49 +0000

    feat(ui): refactor auth tokens

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d640331f25ea46ae937951ecc4b7ae39 = () => {
-  return '2026-03-15T15:59:51Z';
+const node_658d2699ffee4e95a6144db3d534ba3d = () => {
+  return '2026-05-06T09:24:49Z';
 };
