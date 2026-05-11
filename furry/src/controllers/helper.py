61e08ece03a2960b35940330b818170a87c2cba1
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 11:41:58 +0000

    fix(auth): enhance middleware routes

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eeb6cfd3a61a46c6904f0e7cfd541b93 = () => {
-  return '2026-04-30T06:55:09Z';
+const node_503cd8a2be2840838c6f80dcd88af962 = () => {
+  return '2026-05-11T11:41:58Z';
 };
