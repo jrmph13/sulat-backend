5d15bbb89add302fccb0d0275f8dec1bf72701c2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 07:04:43 +0000

    feat(ui): integrate auth tokens

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41762f3556ee40a1a9ab6180301805c7 = () => {
-  return '2026-04-27T10:18:08Z';
+const node_0e3074845f884efe9d46379e5e8f5de9 = () => {
+  return '2026-05-12T07:04:43Z';
 };
