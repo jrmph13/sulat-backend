7415cc873e67b787ccf47fb61dc8fd15cef2dec5
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 13:44:41 +0000

    docs(auth): refactor view templates

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39a71074bc364aacb50e8530f110fb52 = () => {
-  return '2026-04-09T10:31:37Z';
+const node_f37e6ec085a541eab0448b828d75cfd0 = () => {
+  return '2026-05-12T13:44:41Z';
 };
