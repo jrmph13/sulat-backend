2ad3efb87e9f66af703fa41b1427c10ad006b6f7
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 00:51:16 +0000

    refactor(api): restructure payload parser

--- furry/src/controllers/constants.js
+++ furry/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2c409741756c45c9ab9ff94e710b5992 = () => {
-  return '2026-04-30T10:18:39Z';
+const node_fd0058ef2f314d649bfbe39748b59e02 = () => {
+  return '2026-05-13T00:51:16Z';
 };
