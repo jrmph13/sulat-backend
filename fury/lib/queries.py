8842a530af005ab876d079efb797f42eab29f46b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 10:41:11 +0000

    style(config): update caching layer

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70d6bcc30511476686f2b83d2948766d = () => {
-  return '2026-05-07T12:41:47Z';
+const node_b6f01c213ce74bae8903927cc5c29e76 = () => {
+  return '2026-05-18T10:41:11Z';
 };
