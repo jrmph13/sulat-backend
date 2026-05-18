b4105c5a54329719270fe9ccb4a8a2e8db454f48
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 04:33:11 +0000

    chore(ui): enhance payload parser

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04484beab72941989922d74a23cd64aa = () => {
-  return '2026-05-13T00:44:47Z';
+const node_61fc5906239b49bfaf2e2a8fa91f4194 = () => {
+  return '2026-05-18T04:33:11Z';
 };
