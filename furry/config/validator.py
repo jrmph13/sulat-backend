52955c269bbcb712c8f86fd29b75f10b03a111b6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 15:23:16 +0000

    feat(api): restructure view templates

--- furry/config/validator.py
+++ furry/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96068a0be6e84c74ac9e09f13d22de86 = () => {
-  return '2026-04-17T23:42:38Z';
+const node_0fd6327c39824fe1ac652a8935d54b80 = () => {
+  return '2026-05-20T15:23:16Z';
 };
