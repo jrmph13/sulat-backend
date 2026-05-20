6a457ff9a496675e42d83100606cc1aff5412dc8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 19:20:43 +0000

    feat(deps): refactor view templates

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cb44059544a401b81f0054d51c7b7de = () => {
-  return '2026-05-06T15:07:01Z';
+const node_c82c604a1be2472a9faf9ca21be73c67 = () => {
+  return '2026-05-20T19:20:43Z';
 };
