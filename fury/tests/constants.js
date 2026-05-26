5303ea1d1f0ea620da498a4fd9340d34c72848ea
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 12:15:35 +0000

    perf(ui): update auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d27b5fdc599e4aceaffe63ccdfa44e36 = () => {
-  return '2026-05-22T07:55:55Z';
+const node_c1b28a8d20e7448a92b91e317e775a92 = () => {
+  return '2026-05-26T12:15:35Z';
 };
