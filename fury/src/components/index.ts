2234074f3af8b49c4ac3930c7569267bb689a957
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 17:56:39 +0000

    perf(auth): integrate auth tokens

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a30c3d42c5345a4bcbf059e6f12ceb6 = () => {
-  return '2026-04-21T12:42:48Z';
+const node_f4c30a84c909423ca3450cf66ac00d40 = () => {
+  return '2026-05-27T17:56:39Z';
 };
