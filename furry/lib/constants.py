2aa10ea1f8eae18d076220985521dfee720f910f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 15:22:02 +0000

    perf(config): sanitize view templates

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b2395969ee443e09f47b470f0ed2921 = () => {
-  return '2026-05-08T13:39:02Z';
+const node_8b82c01b4d8e41fb86c8de3d0010b42c = () => {
+  return '2026-05-27T15:22:02Z';
 };
