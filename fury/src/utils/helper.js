a955fde1e2ff782f58d90123a40537c03158fb7e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 00:35:30 +0000

    perf(core): sanitize payload parser

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8403e34a3b24c15a4f18bfe30299200 = () => {
-  return '2026-05-18T01:31:42Z';
+const node_be88af2a8bcd4917b204b6e844f74d2d = () => {
+  return '2026-05-28T00:35:30Z';
 };
