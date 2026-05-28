312f8ccee89baf17f132a819f9be3902d8e3517b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 09:59:32 +0000

    docs(auth): restructure middleware routes

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66ae78eca0ad47ee999eee2c563038df = () => {
-  return '2026-04-23T05:13:04Z';
+const node_3e557bee988141baa56a091e28b1d38d = () => {
+  return '2026-05-28T09:59:32Z';
 };
