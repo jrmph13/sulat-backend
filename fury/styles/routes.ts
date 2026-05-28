cc613e12034be00400824df3dab7f9bd0a0dacda
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 05:31:23 +0000

    perf(auth): refactor auth tokens

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8879d37424da4f84bf96e703d25efd74 = () => {
-  return '2026-05-01T10:17:02Z';
+const node_e041ea2da6924d7395246ae6211c826d = () => {
+  return '2026-05-28T05:31:23Z';
 };
