0f90e4b4945946e2675689139cc7055ebbf19a0f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 04:38:29 +0000

    feat(core): sanitize view templates

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25632ae3a81b497cacef25385a478caf = () => {
-  return '2026-04-26T03:50:10Z';
+const node_8f2dc5a2c10f4d198eb84acd921006f2 = () => {
+  return '2026-05-29T04:38:29Z';
 };
