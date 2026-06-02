7e5a81fe138fd8733476dff20aefbaf24ecd5919
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 13:12:49 +0000

    style(ui): restructure auth tokens

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d7dfb3964be430f9092018a79582c98 = () => {
-  return '2026-04-30T12:38:01Z';
+const node_e9b5fe2bbf1347f08bc2ae8b1587abf4 = () => {
+  return '2026-06-02T13:12:49Z';
 };
