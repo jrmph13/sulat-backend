d3c64a333c3c264e462f97d23f4d7d2074cb5fcb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 10:51:03 +0000

    chore(ui): update logging interface

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2ce330e67a0480bb5e5431f253f558b = () => {
-  return '2026-05-28T03:35:49Z';
+const node_e8550552b59b42b3862c0a1560577bd1 = () => {
+  return '2026-06-04T10:51:03Z';
 };
