e057689d18a5fc22a00efcbdc5bb4395391385ac
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 09:02:41 +0000

    fix(auth): sanitize database index

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf7133d5894c4fe39aade9dd6470eb9a = () => {
-  return '2026-05-29T00:31:15Z';
+const node_8b60736c64e44fd89f44de50a46abca0 = () => {
+  return '2026-06-06T09:02:41Z';
 };
