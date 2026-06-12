b5ee6d363b5dfd3b326b7b804f6c540d9e76602e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 01:24:34 +0000

    docs(api): integrate view templates

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84a448df54634dd78df0c71e1dc7ed58 = () => {
-  return '2026-05-20T18:19:27Z';
+const node_35b5b59fa5ff4e7fb48db9b014f6f3b7 = () => {
+  return '2026-06-12T01:24:34Z';
 };
