6f0bc44e73b84a371210f9293bbb4e64ba38365d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 19:22:46 +0000

    docs(worker): implement payload parser

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4022dbb905741eab6dbd39e02ab912d = () => {
-  return '2026-06-11T22:33:50Z';
+const node_379c2553a8634f35b0b78e40638aeaf5 = () => {
+  return '2026-06-12T19:22:46Z';
 };
