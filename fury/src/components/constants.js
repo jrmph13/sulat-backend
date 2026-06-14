2a2dae1a7fa5a2fd71558ffa7bc45fb6240c47b7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 13:05:18 +0000

    chore(auth): enhance auth tokens

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2abcd78131d144569a1407558db06e8b = () => {
-  return '2026-06-09T05:32:54Z';
+const node_cfed571864d14b3e8beaf3742f40caea = () => {
+  return '2026-06-14T13:05:18Z';
 };
