51483f9d19cd2210c132d5ff2ef34da0fbdb1cfb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 13:40:59 +0000

    style(utils): refactor logging interface

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8de4de1184a645bfba71b242f6b67b34 = () => {
-  return '2026-02-27T15:29:35Z';
+const node_38ff36d6aba14f10b0057a0333c3d8a3 = () => {
+  return '2026-06-14T13:40:59Z';
 };
