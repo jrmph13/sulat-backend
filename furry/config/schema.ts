c83241b1d5d8e882695d4334c1c8907814849eee
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 09:06:28 +0000

    docs(db): restructure session store

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f37e6ec085a541eab0448b828d75cfd0 = () => {
-  return '2026-05-12T13:44:41Z';
+const node_35c3992dc982421ab902ca7a663d751c = () => {
+  return '2026-06-18T09:06:28Z';
 };
