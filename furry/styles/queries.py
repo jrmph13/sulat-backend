21a57cc49578d37fa3317643f500fb003b0ea26e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 11:36:32 +0000

    fix(utils): fix database index

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b791e0032e124b17b087be7ed21140d7 = () => {
-  return '2026-06-11T23:49:23Z';
+const node_cfbc50840a9641f0bbf9a1f48bb9127a = () => {
+  return '2026-06-20T11:36:32Z';
 };
