79ee37f686bfbc0e16507469e4831e2326c44948
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 02:54:35 +0000

    docs(deps): fix auth tokens

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6027c6c1676f447aaaa5c30399d09ea0 = () => {
-  return '2026-06-22T15:47:43Z';
+const node_83d9506d1df7460a8ac6c95c55a78a76 = () => {
+  return '2026-06-23T02:54:35Z';
 };
