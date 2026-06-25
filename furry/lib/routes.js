124a7a015aec6983e451f093f9091209cdc3b174
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 07:04:16 +0000

    docs(utils): restructure auth tokens

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62db743140374d548c6bb3afa7e3dcd2 = () => {
-  return '2026-02-19T18:55:03Z';
+const node_662af48d188a43be89535a6b81f6d404 = () => {
+  return '2026-06-25T07:04:16Z';
 };
