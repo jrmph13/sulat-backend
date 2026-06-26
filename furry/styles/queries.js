ae13628bdf32198340e821043313785d6e40eaad
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 07:25:55 +0000

    fix(config): fix view templates

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f29497e351a94878ba0ecf46ab4a2bfc = () => {
-  return '2026-06-09T18:09:12Z';
+const node_6d59589a63e24fb5b52c88056882f061 = () => {
+  return '2026-06-26T07:25:55Z';
 };
